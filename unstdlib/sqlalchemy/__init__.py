1277fe4bf25b15fdfc5ccbaf965dd126252b2bb3
Andrey Petrov <andrey.petrov@shazow.net>
2011-06-29 13:50:31 -0400

    Yield individual results for enumerate_query_by_limit

--- unstdlib/sqlalchemy/__init__.py
+++ unstdlib/sqlalchemy/__init__.py
@@ -6,11 +6,14 @@ __all__ = ['enumerate_query_by_limit']
 
 def enumerate_query_by_limit(q, limit=1000):
     """
-    Enumerate over SQLAlchemy query object ``q`` and yield results in batches of
-    size ``limit`` using limit and offset.
+    Enumerate over SQLAlchemy query object ``q`` and yield individual results
+    fetched in batches of size ``limit`` using SQL LIMIT and OFFSET.
     """
     for offset in count(0, limit):
         r = q.offset(offset).limit(limit).all()
-        yield r
+
+        for row in r:
+            yield row
+
         if len(r) < limit:
             break
