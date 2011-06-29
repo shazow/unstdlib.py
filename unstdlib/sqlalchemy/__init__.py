81845c8ad2854c78b2966c3261207420e135d0b8
Andrey Petrov <andrey.petrov@shazow.net>
2011-06-29 13:17:33 -0400

    Adding unstdlib.sqlalchemy.enumerate_query_by_limit

---
+++ unstdlib/sqlalchemy/__init__.py
@@ -0,0 +1,13 @@
+from itertools import count
+
+
+def enumerate_query_by_limit(q, limit=1000):
+    """
+    Enumerate over SQLAlchemy query object ``q`` and yield results in batches of
+    size ``limit`` using limit and offset.
+    """
+    for offset in count(0, limit):
+        r = q.offset(offset).limit(limit).all()
+        yield r
+        if len(r) < limit:
+            break
