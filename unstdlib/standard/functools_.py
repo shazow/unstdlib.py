9b17c883d03f0f4d343423e2b8de89f0a8997176
Andrey Petrov <andrey.petrov@shazow.net>
2013-03-09 13:45:36 -0800

    Adding zzzeeks memoized_property

---
+++ unstdlib/standard/functools_.py
@@ -0,0 +1,14 @@
+# memoized_property is lovingly borrowed from @zzzeek, with permission:
+#   https://twitter.com/zzzeek/status/310503354268790784
+class memoized_property(object):
+    """A read-only @property that is only evaluated once."""
+    def __init__(self, fget, doc=None):
+        self.fget = fget
+        self.__doc__ = doc or fget.__doc__
+        self.__name__ = fget.__name__
+
+    def __get__(self, obj, cls):
+        if obj is None:
+            return self
+        obj.__dict__[self.__name__] = result = self.fget(obj)
+        return result
