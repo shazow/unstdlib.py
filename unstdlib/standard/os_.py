e0a46efd0f6629a92082ca52aca4978952fcbea7
David Wolever <david@wolever.net>
2013-08-28 13:11:44 -0400

    Adding chdir context manager

---
+++ unstdlib/standard/os_.py
@@ -0,0 +1,49 @@
+import os
+
+class chdir(object):
+    """ A drop-in replacement for ``os.chdir`` which also acts as a context
+        manager.
+
+        >>> old_cwd = os.getcwd()
+        >>> with chdir("/usr/"):
+        ...     print "current dir:", os.getcwd()
+        ...
+        current dir: /usr
+        >>> os.getcwd() == old_cwd
+        True
+        >>> x = chdir("/usr/")
+        >>> os.getcwd()
+        '/usr'
+        >>> x
+        chdir('/usr/', old_path='...')
+        >>> x.unchdir()
+        >>> os.getcwd() == old_cwd
+        True
+        """
+
+    def __init__(self, new_path, old_path=None):
+        self.old_path = old_path or os.getcwd()
+        self.new_path = new_path
+        os.chdir(self.new_path)
+
+    def __enter__(self):
+        self.chdir()
+        return self
+
+    def __exit__(self, *args):
+        self.unchdir()
+
+    def chdir(self):
+        os.chdir(self.new_path)
+
+    def unchdir(self):
+        os.chdir(self.old_path)
+
+    def __repr__(self):
+        return "%s(%r, old_path=%r)" %(
+            type(self).__name__, self.new_path, self.old_path,
+        )
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testmod(optionflags=doctest.ELLIPSIS)
