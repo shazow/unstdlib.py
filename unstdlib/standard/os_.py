8581d1a3d1aaa52fc20e7ca6e75f3b2b9dbffcb6
Tasos Latsas <tlatsas@gmx.com>
2013-11-24 03:06:06 +0200

    fix print statement

--- unstdlib/standard/os_.py
+++ unstdlib/standard/os_.py
@@ -8,7 +8,7 @@ class chdir(object):
 
         >>> old_cwd = os.getcwd()
         >>> with chdir("/usr/"):
-        ...     print "current dir:", os.getcwd()
+        ...     print("current dir: {0}".format(os.getcwd()))
         ...
         current dir: /usr
         >>> os.getcwd() == old_cwd
